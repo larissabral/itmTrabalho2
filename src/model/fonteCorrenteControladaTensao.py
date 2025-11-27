c7bc34d1e7f024334d3c39eab986cff2b90d0e02
larissa <larissabral@poli.ufrj.br>
2025-11-27 11:25:58 -0300

    Cria classe modelo para fonte de corrente controlada por tensão
    [Issue: https://github.com/larissabral/itmTrabalho2/issues/2]

---
+++ src/model/fonteCorrenteControladaTensao.py
@@ -0,0 +1,31 @@
+class FonteCorrenteControladaTensao:
+    def __init__(
+        self,
+        noCorrentePositivo,
+        noCorrenteNegativo,
+        noControlePositivo,
+        noControleNegativo,
+        transcondutancia,
+    ):
+        self.noCorrentePositivo = noCorrentePositivo
+        self.noCorrenteNegativo = noCorrenteNegativo
+        self.noControlePositivo = noControlePositivo
+        self.noControleNegativo = noControleNegativo
+        self.transcondutancia = transcondutancia
+
+    def to_nl(self):
+        return [
+            "G",
+            self.noCorrentePositivo,
+            self.noCorrenteNegativo,
+            self.noControlePositivo,
+            self.noControleNegativo,
+            self.transcondutancia,
+        ]
+
+    def from_nl(self, nl):
+        self.noCorrentePositivo = int(nl[1])
+        self.noCorrenteNegativo = int(nl[2])
+        self.noControlePositivo = int(nl[3])
+        self.noControleNegativo = int(nl[4])
+        self.transcondutancia = int(nl[5])

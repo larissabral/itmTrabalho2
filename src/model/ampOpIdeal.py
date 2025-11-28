0c1d11f3cc4935db0647fb516d6fc52bbf5f2ce8
larissa <larissabral@poli.ufrj.br>
2025-11-28 12:40:34 -0300

    Adapta classe do ampOp para estender classe base
    Falta implementar estampa
    
    [Issue: https://github.com/larissabral/itmTrabalho2/issues/2]

--- src/model/ampOpIdeal.py
+++ src/model/ampOpIdeal.py
@@ -1,13 +1,19 @@
-class AmpOpIdeal:
-    def __init__(self, noControlePositivo, noControleNegativo, noSaida):
-        self.noControlePositivo = noControlePositivo
-        self.noControleNegativo = noControleNegativo
+from src.model.elementoCircuito import ElementoCircuito
+
+
+class AmpOpIdeal(ElementoCircuito):
+    def __init__(self, noPositivo, noNegativo, noSaida):
+        super().__init__(noPositivo, noNegativo)
         self.noSaida = noSaida
 
     def to_nl(self):
-        return ["O", self.noControlePositivo, self.noControleNegativo, self.noSaida]
+        return ["O", self.noPositivo, self.noNegativo, self.noSaida]
 
     def from_nl(self, nl):
-        self.noControlePositivo = int(nl[1])
-        self.noControleNegativo = int(nl[2])
+        self.noPositivo = int(nl[1])
+        self.noNegativo = int(nl[2])
         self.noSaida = int(nl[3])
+
+    def estampa(self, G, I, deltaT, tensoesAnteriores, correntesAnteriores):
+        # TODO: implementar estampa do ampop
+        raise NotImplementedError

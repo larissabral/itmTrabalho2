ec4277f3ccae670676231111f709a9f14e4058ee
larissa <larissabral@poli.ufrj.br>
2025-11-28 13:09:10 -0300

    Adapta classe do resistor não linear para estender classe base e implementar estampa
    [Issue: https://github.com/larissabral/itmTrabalho2/issues/2]

--- src/model/resistorNaoLinear.py
+++ src/model/resistorNaoLinear.py
@@ -1,7 +1,11 @@
-class ResistorNaoLinear:
+import numpy as np
+
+from src.model.elementoCircuito import ElementoCircuito
+
+
+class ResistorNaoLinear(ElementoCircuito):
     def __init__(self, noPositivo, noNegativo, v1, v2, v3, v4, i1, i2, i3, i4):
-        self.noPositivo = noPositivo
-        self.noNegativo = noNegativo
+        super().__init__(noPositivo, noNegativo)
         self.v1 = v1
         self.v2 = v2
         self.v3 = v3
@@ -37,3 +41,38 @@ class ResistorNaoLinear:
         self.i3 = float(nl[8])
         self.v4 = float(nl[9])
         self.i4 = float(nl[10])
+
+    def estampa(
+        self, G, I, deltaT, tensoesAnteriores, correntesAnteriores, posicao, qntNos
+    ):
+        noA = self.noPositivo
+        noB = self.noNegativo
+        G0 = 0
+        I0 = 0
+
+        tensao = tensoesAnteriores[noA] - tensoesAnteriores[noB]
+
+        if tensao > self.v3:
+            G0 = (self.i4 - self.i3) / (self.v4 - self.v3)
+            I0 = self.i4 - self.v4
+
+        if self.v2 < tensao <= self.v3:
+            G0 = (self.i3 - self.i2) / (self.v3 - self.v2)
+            I0 = self.i3 - self.v3
+
+        if tensao <= self.v2:
+            G0 = (self.i2 - self.i1) / (self.v2 - self.v1)
+            I0 = self.i2 - self.v2
+
+        if G0 == 0:
+            resistencia = np.inf
+        else:
+            resistencia = 1 / G0
+
+        G[noA, noA] += resistencia
+        G[noA, noB] -= resistencia
+        G[noB, noA] -= resistencia
+        G[noB, noB] += resistencia
+
+        I[noA] += I0
+        I[noB] -= I0

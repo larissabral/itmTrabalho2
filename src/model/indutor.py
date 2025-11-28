65acd5c5cd44bd3575f4257684a4afe570a55455
larissa <larissabral@poli.ufrj.br>
2025-11-28 12:51:22 -0300

    Adapta classe do indutor para estender classe base e implementar estampa
    [Issue: https://github.com/larissabral/itmTrabalho2/issues/2]

--- src/model/indutor.py
+++ src/model/indutor.py
@@ -1,7 +1,9 @@
-class Indutor:
+from src.model.elementoCircuito import ElementoCircuito
+
+
+class Indutor(ElementoCircuito):
     def __init__(self, noPositivo, noNegativo, indutancia, correnteInicial):
-        self.noPositivo = noPositivo
-        self.noNegativo = noNegativo
+        super().__init__(noPositivo, noNegativo)
         self.indutancia = indutancia
         self.correnteInicial = correnteInicial
 
@@ -19,3 +21,20 @@ class Indutor:
         self.noNegativo = int(nl[2])
         self.indutancia = float(nl[3])
         self.correnteInicial = float(nl[4].replace("IC=", ""))
+
+    def estampa(
+        self, G, I, deltaT, tensoesAnteriores, correntesAnteriores, posicao, qntNos
+    ):
+        noA = self.noPositivo
+        noB = self.noNegativo
+
+        indutancia_equivalente = self.indutancia / deltaT
+
+        G[noA, posicao] += 1
+        G[noB, posicao] -= 1
+        G[posicao, noA] -= 1
+        G[posicao, noB] += 1
+        G[posicao, posicao] += indutancia_equivalente
+
+        i_t0 = correntesAnteriores[posicao - qntNos] * indutancia_equivalente
+        I[posicao] += i_t0

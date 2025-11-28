a51865ae04561bf4be0f3132755941fd1fbec7a4
larissa <larissabral@poli.ufrj.br>
2025-11-28 12:06:20 -0300

    Cria classe base para todos os elementos
    [Issue: https://github.com/larissabral/itmTrabalho2/issues/2]

---
+++ src/model/elementoCircuito.py
@@ -0,0 +1,18 @@
+from abc import ABC, abstractmethod
+
+
+class ElementoCircuito(ABC):
+    def __init__(self, noPositivo, noNegativo):
+        self.noPositivo = noPositivo
+        self.noNegativo = noNegativo
+
+    @abstractmethod
+    def estampa(self, G, I, deltaT, tensoesAnteriores, correntesAnteriores):
+        """
+
+        - G: matriz de condutâncias
+        - I: vetor de correntes
+        - deltaT: passo de integração
+
+        """
+        pass

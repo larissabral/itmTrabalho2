506dc8884cce94d3a62b8d49c00ef91e96b9b6b2
larissa <larissabral@poli.ufrj.br>
2025-11-28 12:27:48 -0300

    Adapta classe do diodo para estender classe base e implementar estampa
    [Issue: https://github.com/larissabral/itmTrabalho2/issues/2]

--- src/model/diodo.py
+++ src/model/diodo.py
@@ -1,7 +1,11 @@
-class Diodo:
+import numpy as np
+
+from src.model.elementoCircuito import ElementoCircuito
+
+
+class Diodo(ElementoCircuito):
     def __init__(self, noPositivo, noNegativo):
-        self.noPositivo = noPositivo
-        self.noNegativo = noNegativo
+        super().__init__(noPositivo, noNegativo)
 
     def to_nl(self):
         return ["D", self.noPositivo, self.noNegativo]
@@ -9,3 +13,33 @@ class Diodo:
     def from_nl(self, nl):
         self.noPositivo = int(nl[1])
         self.noNegativo = int(nl[2])
+
+    def estampa(self, G, I, deltaT, tensoesAnteriores, correntesAnteriores):
+        noA = self.noPositivo
+        noB = self.noNegativo
+
+        Is = 3.7751345e-14
+        Vt = 25.0e-3
+
+        tensao = tensoesAnteriores[noA] - tensoesAnteriores[noB]
+        if tensao > 0.9:
+            tensao = 0.9
+
+        i = Is * np.exp(tensao / Vt)
+
+        condutancia = i / Vt
+        if condutancia == 0:
+            G0 = np.inf
+        else:
+            G0 = 1.0 / condutancia
+
+        resistencia = 1 / G0
+        corrente = i - Is - (condutancia * tensao)
+
+        G[noA, noA] += resistencia
+        G[noA, noB] -= resistencia
+        G[noB, noA] -= resistencia
+        G[noB, noB] += resistencia
+
+        I[noA] -= corrente
+        I[noB] += corrente

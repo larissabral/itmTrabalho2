eb7bd87e2089650700146f4e399ec01c5362b4f7
larissa <larissabral@poli.ufrj.br>
2025-11-28 12:07:01 -0300

    Adapta classe do capacitor para estender classe base e implementar estampa
    [Issue: https://github.com/larissabral/itmTrabalho2/issues/2]

--- src/model/capacitor.py
+++ src/model/capacitor.py
@@ -1,7 +1,9 @@
-class Capacitor:
+from src.model.elementoCircuito import ElementoCircuito
+
+
+class Capacitor(ElementoCircuito):
     def __init__(self, noPositivo, noNegativo, capacitancia, tensaoInicial):
-        self.noPositivo = noPositivo
-        self.noNegativo = noNegativo
+        super().__init__(noPositivo, noNegativo)
         self.capacitancia = capacitancia
         self.tensaoInicial = tensaoInicial
 
@@ -19,3 +21,19 @@ class Capacitor:
         self.noNegativo = int(nl[2])
         self.capacitancia = float(nl[3])
         self.tensaoInicial = float(nl[4].replace("IC=", ""))
+
+    def estampa(self, G, I, deltaT, tensoesAnteriores, correntesAnteriores):
+        noA = self.noPositivo
+        noB = self.noNegativo
+        capacitancia_equivalente = self.capacitancia / deltaT
+        corrente = capacitancia_equivalente * (
+            tensoesAnteriores[noA] - tensoesAnteriores[noB]
+        )
+
+        G[noA, noA] += capacitancia_equivalente
+        G[noA, noB] -= capacitancia_equivalente
+        G[noB, noA] -= capacitancia_equivalente
+        G[noB, noB] += capacitancia_equivalente
+
+        I[noA] += corrente
+        I[noB] -= corrente

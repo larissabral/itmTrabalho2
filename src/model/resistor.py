79de563b6e6941c08bfbaafa407c1e14063e38d2
larissa <larissabral@poli.ufrj.br>
2025-11-28 12:39:50 -0300

    Adapta classe do resistor para estender classe base e implementar estampa
    [Issue: https://github.com/larissabral/itmTrabalho2/issues/2]

--- src/model/resistor.py
+++ src/model/resistor.py
@@ -1,8 +1,10 @@
-class Resistor:
+from src.model.elementoCircuito import ElementoCircuito
+
+
+class Resistor(ElementoCircuito):
     def __init__(self, resistencia, noPositivo, noNegativo):
+        super().__init__(noPositivo, noNegativo)
         self.resistencia = resistencia
-        self.noPositivo = noPositivo
-        self.noNegativo = noNegativo
 
     def to_nl(self):
         return ["R", self.noPositivo, self.noNegativo, self.resistencia]
@@ -11,3 +13,13 @@ class Resistor:
         self.resistencia = float(nl[3])
         self.noPositivo = int(nl[1])
         self.noNegativo = int(nl[2])
+
+    def estampa(self, G, I, deltaT, tensoesAnteriores, correntesAnteriores):
+        noA = self.noPositivo
+        noB = self.noNegativo
+        condutancia = 1 / self.resistencia
+
+        G[noA, noA] += condutancia
+        G[noA, noB] -= condutancia
+        G[noB, noA] -= condutancia
+        G[noB, noB] += condutancia

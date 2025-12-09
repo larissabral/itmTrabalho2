63e050b34b3778f55d203b20a9fb301a26e2a515
larissa <larissabral@poli.ufrj.br>
2025-12-08 23:39:36 -0300

    Corrige leitura do gabarito dos testes para desconsiderar primeira linha de cabeçalho
    [Issue: https://github.com/larissabral/itmTrabalho2/issues/15]

--- src/tests/test_dc_source.py
+++ src/tests/test_dc_source.py
@@ -13,5 +13,6 @@ def test_dc_source():
 
     resultados = Simulador().simular_from_nl(netlist_path)
 
-    with open(gabarito_path) as gabarito:
-        assert np.array_equal(resultados, gabarito.read())
+    gabarito = np.loadtxt(gabarito_path, skiprows=1)
+
+    assert np.allclose(resultados, gabarito, atol=1e-6)
